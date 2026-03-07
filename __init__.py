637043ffd44e3e65d22cef404995dc9764355dfe
deminhu <demin.hu.22@ucl.ac.uk>
2026-03-07 12:33:56 +0000

    Add serverClassFactory and rename plugin folder to ssjgraph

--- __init__.py
+++ __init__.py
@@ -1,3 +1,8 @@
 def classFactory(iface):
     from .jgraph_plugin import JGraphPlugin
     return JGraphPlugin(iface)
+
+
+def serverClassFactory(serverIface):
+    from .jgraph_plugin import JGraphPlugin
+    return JGraphPlugin(serverIface)
